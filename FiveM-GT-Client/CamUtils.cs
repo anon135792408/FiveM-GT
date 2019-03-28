6e06751d91d03c51b9b652d586b20ad70b6fbde6
Zulfurix <zulfurix@gmail.com>
2019-03-28 22:37:18 +0000

    Implement IsIntroCamRunning boolean

--- FiveM-GT-Client/CamUtils.cs
+++ FiveM-GT-Client/CamUtils.cs
@@ -10,16 +10,21 @@ namespace FiveM_GT_Client
 {
     class CamUtils : BaseScript
     {
+        private static bool isIntroCamRunning = false;
+
+        public static bool IsIntroCamRunning { get { return isIntroCamRunning; } }
+
         public static async void InitRaceStartCam()
         {
             if (Game.PlayerPed != null && Game.PlayerPed.IsInVehicle())
             {
                 Debug.WriteLine("[FiveM-GT] Initialising Race Start Cam...");
 
+                isIntroCamRunning = true;
+
                 Vector3 posInFront = Game.PlayerPed.Position + (Game.PlayerPed.ForwardVector * 20) + (Game.PlayerPed.UpVector * 2) - (Game.PlayerPed.RightVector + 1);
                 Camera camStart = World.CreateCamera(posInFront, Vector3.Zero, 50f);
                 camStart.PointAt(Game.PlayerPed.Position);
-                //RenderScriptCams(true, false, 0, true, true);
 
                 Vector3 posBeside = Game.PlayerPed.Position + (Game.PlayerPed.ForwardVector * 5) + (Game.PlayerPed.UpVector * 2) + (Game.PlayerPed.RightVector * 2);
                 Camera camEnd = World.CreateCamera(posBeside, Vector3.Zero, 50f);
@@ -29,11 +34,8 @@ namespace FiveM_GT_Client
 
                 camStart.InterpTo(camEnd, 8000, true, true);
 
-                while (camStart.IsInterpolating)
-                {
-                    await Delay(0);
-                }
-                //RenderScriptCams(false, false, 0, true, true);
+                await Delay(8000);
+                isIntroCamRunning = false;
             }
         }
     }
